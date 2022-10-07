e321bee3d8878a73394c31ed96edb8b3edb13fe9
ChrisMCodes <34985965+ChrisMCodes@users.noreply.github.com>
2022-10-07 08:57:57 -0700

    Update moreReferencePractice.cpp

--- moreReferencePractice.cpp
+++ moreReferencePractice.cpp
@@ -9,16 +9,16 @@ int main()
   int &r = someLongVariableName; // "r" is now a nickname for "a"
   
   cout << "Initial someLongVariableName: " << someLongVariableName << endl; // 10
-  cout << "Initial r: " << r << endl << endl; // 10
+  cout << "Initial r:                    " << r << endl << endl; // 10
   
   // references are bound to the variable they reference!
 
   int b = 25;
   r = b; // someLongVariableName will be 25
   
-  cout << "r was set to b. b: " << b << endl; // 25
+  cout << "r was set to b. b:    " << b << endl; // 25
   cout << "someLongVariableName: " << someLongVariableName << endl; // 25
-  cout << "r: " << r << endl; // 25
+  cout << "r:                    " << r << endl; // 25
 
   return 0;
 }
